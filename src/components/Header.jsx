5c351edc1871bc59cabacd59dc03f0a3d7d71b20
Manish Sahani <manishsahani345@gmail.com>
2024-11-10 09:53:30 +0530

    Update Header.jsx
    typo error resolve

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 // import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import "../styles/Header.css";
-import myResume from "../assets/src/assets/MANISH_SAHANI_RESUME_2024.pdf.pdf";
+import myResume from "../assets/src/assets/MANISH_SAHANI_RESUME_2024.pdf";
 import ScrollIndicator from './Scroll_Indicator';
 
 
