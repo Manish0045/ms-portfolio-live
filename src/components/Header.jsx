d5ec77c3580c4f94a7dbd6186bcc84b8253f1493
Manish Sahani <manishsahani345@gmail.com>
2024-11-10 09:39:04 +0530

    Update Header.jsx
    Resume change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 // import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import "../styles/Header.css";
-import myResume from "../assets/MANISH_RESUME_2024.pdf";
+import myResume from "../assets/Manish Sahani_2024.pdf";
 import ScrollIndicator from './Scroll_Indicator';
 
 
